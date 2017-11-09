d8a3a059ba0904ba70652bdac6cd841aa8e80861
Rahul <rahulnanda.iiti@gmail.com>
2017-11-08 21:41:40 -0500

    Written on Omkar's machine. Examples provided by Lakshay

---
+++ split.py
@@ -0,0 +1,16 @@
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+def split():
+    #fx = np.loadtxt(fname = "small_loan_x.csv", delimiter = ',')
+    #fy = np.loadtxt(fname = "small_loan_y.csv", delimiter = ',')
+
+    fx = np.loadtxt(fname = "loan_x_final copy.csv", delimiter = ',')
+    fy = np.loadtxt(fname = "loan_y_final copy.csv", delimiter = ',')
+
+    X = fx[:, 1:]
+    y = fy[:, 1:]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+
+    return X_train, X_test, y_train, y_test
