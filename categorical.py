ae79524cf59612cba3a1807dd0c841b30df074e1
Rahul <rahulnanda.iiti@gmail.com>
2017-11-10 00:16:54 -0500

    minor changes

--- categorical.py
+++ categorical.py
@@ -4,24 +4,22 @@ import math
 import numpy as np
 
 # Read in the CSV file and convert "?" to NaN
-MONTHS = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+
 
 def toNumericFromMonth(month):
     if type(month) is not float:
+        months=['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
         monthsval1,monthsval2 = month.split("-")
-        ans = MONTHS.index(monthsval1)+int(monthsval2)*12
+        ans = months.index(monthsval1)+int(monthsval2)*12
         return ans
     else:
         return 0
 
-
 def removeMonths(month):
     return str(month).split(" ")[0]
 
-
 def removePercentage(percentage):
     return float(str(percentage).split("%")[0])
-
 
 def extractNums(mixed):
     return filter(str.isdigit, str(mixed))
@@ -34,16 +32,19 @@ def ws_to_zero(maybe_ws):
         except:
             return 0
     if isinstance(maybe_ws, float) and np.isnan(maybe_ws):
-        print maybe_ws
         return 0
     return maybe_ws
 
 df = pd.read_csv("/Users/omkar/Downloads/merged.csv")
 df.head()
 
-categorical_columns=["addr_state", "application_type", "purpose",
-                     "grade","home_ownership", "sub_grade", "fico_range_high",
-                     "fico_range_low", "pymnt_plan", "verification_status", "loan_status"]
+
+df = df[(df["loan_status"]=="Default") | (df["loan_status"]=="Charged Off") | (df["loan_status"]=="Fully Paid")]
+
+
+categorical_columns=["addr_state","application_type","purpose",
+                     "grade","home_ownership","sub_grade","fico_range_high",
+                     "fico_range_low","pymnt_plan","verification_status","loan_status"]
 
 month_columns = ["earliest_cr_line","last_credit_pull_d","last_pymnt_d"]
 
@@ -51,6 +52,8 @@ percentage_columns=["int_rate","revol_util"]
 
 df['term_updated'] = df['term'].apply(lambda x : removeMonths(x))
 df['emp_length_updated'] = df['emp_length'].apply(lambda x : extractNums(x))
+
+
 
 for val in percentage_columns:
     df[val+'_updated'] = df[val].apply(lambda x: removePercentage(x))
@@ -67,31 +70,17 @@ for category in categorical_columns:
     df[category + '_updated'] +=1
 
 
-df_updated = df[["id","addr_state_updated","annual_inc","annual_inc_joint","application_type_updated","delinq_2yrs",
-                 "dti","dti_joint","earliest_cr_line_updated","funded_amnt","funded_amnt_inv","grade_updated",
-                 "home_ownership_updated","int_rate_updated","last_credit_pull_d_updated","fico_range_high_updated",
-                 "fico_range_low_updated","last_pymnt_amnt","last_pymnt_d_updated","loan_amnt","open_acc",
-                 "purpose_updated","pymnt_plan_updated","revol_bal","revol_util_updated","sub_grade_updated",
-                 "verification_status_updated","term_updated","total_acc","total_pymnt","total_pymnt_inv",
-                 "total_rec_late_fee","il_util","max_bal_bc","acc_now_delinq","tot_coll_amt","tot_cur_bal",
-                 "emp_length_updated"]]
 
-'''selected_columns= ["id","addr_state_updated","annual_inc","annual_inc_joint","application_type_updated","delinq_2yrs","dti","dti_joint","earliest_cr_line_updated","funded_amnt","funded_amnt_inv","grade_updated","home_ownership_updated","int_rate_updated","last_credit_pull_d_updated","fico_range_high_updated","fico_range_low_updated","last_pymnt_amnt","last_pymnt_d_updated","loan_amnt","open_acc","purpose_updated","pymnt_plan_updated","revol_bal","revol_util_updated","sub_grade_updated","verification_status_updated","term_updated","total_acc","total_pymnt","total_pymnt_inv","total_rec_late_fee","il_util","max_bal_bc","acc_now_delinq","tot_coll_amt","tot_cur_bal","emp_length_updated"]
-x = pd.DataFrame()p
+df_updated = df[["addr_state_updated","annual_inc","application_type_updated","delinq_2yrs","dti","earliest_cr_line_updated","funded_amnt","funded_amnt_inv","grade_updated","home_ownership_updated","int_rate_updated","last_credit_pull_d_updated","fico_range_high_updated","fico_range_low_updated","last_pymnt_amnt","last_pymnt_d_updated","loan_amnt","open_acc","purpose_updated","pymnt_plan_updated","revol_bal","revol_util_updated","sub_grade_updated","verification_status_updated","term_updated","total_acc","total_pymnt","total_pymnt_inv","total_rec_late_fee","acc_now_delinq","tot_coll_amt","tot_cur_bal","emp_length_updated"]]
 
-for column in selected_columns:
-    x[column] = df[column].apply(lambda x :ws_to_zero)'''
 
-print df_updated[0:4]
-
-print "-------------"
 df_updated_y=df[["loan_status_updated"]]
 
 df_updated=df_updated.applymap(lambda x: ws_to_zero(x))
 
-print df_updated[0:4]
+
 df_updated.to_csv("loan_x_final.csv")
-df_updated_y.to_csv("loan_y_final.csv")
+df_updated_y.to_csv("loan_y_final.csv");
 
 
 
--- load_data_test.py
+++ load_data_test.py
@@ -1,11 +1,72 @@
 from tabulate import tabulate
 import sqlite3
-# URL for the Pima Indians Diabetes dataset (UCI Machine Learning Repository)
-url = "/Users/omkar/Downloads/lending-club-loan-data/database.sqlite"
-# load the CSV file as a numpy matrix
-conn = sqlite3.connect(url)
-query = '''select count(distinct(id)) as count from loan'''
+#url = "/Users/rahul/Downloads/lending-club-loan-data/database.sqlite"
+url_x = "/Users/rahul/Downloads/loan_x_final.csv"
+url_y = "/Users/rahul/Downloads/loan_y_final.csv"
 
-# Creating dataframe from query.Teams = conn.execute(query).fetchall()
-Teams = conn.execute(query).fetchall()
-print tabulate(Teams, headers='keys', tablefmt='psql')
+f_x, f_y = open(url_x, 'r'), open(url_y, 'r')
+f_x1, f_y1 = open(url_x+'_modified', 'w'), open(url_y+'_modified', 'w')
+
+lines_x, lines_y = f_x.readlines(), f_y.readlines()
+print len(lines_x), len(lines_y)
+print lines_x[0]
+print lines_y[0]
+default_row = []
+for i, xy in enumerate(zip(lines_x, lines_y)):
+    #print xy[1].strip('\n')
+    if xy[1].split(',')[1].strip('\n') == '2':
+        default_row.append(i)
+        print xy[1]
+    else:
+        f_x1.write(xy[0])
+        f_y1.write(xy[1])
+    # if i == 10:
+    #     break
+for i in default_row:
+    f_x1.write(lines_x[i])
+    f_y1.write(lines_y[i])
+print default_row
+f_x.close()
+f_y.close()
+f_x1.close()
+f_y1.close()
+
+#print lines[1].index('fico_range_high')
+# lines[1] = [x.strip('\"') for x in lines[1].split(',')]
+# for i,x in enumerate(lines[1]):
+#     print i,x
+# for l in lines[1:30]:
+#     #print l
+#     l = [x.strip('\"') for x in l.split(',')]
+#     print l[28]
+# f.close()
+# file_names = ["LoanStats3a_securev1.csv", "LoanStats3b_securev1.csv",
+#               "LoanStats3c_securev1.csv", "LoanStats3d_securev1.csv"]
+#file_names = ["test1","test2"]
+# count = 0
+# wf = open("merged.csv", 'w')
+# with open("../"+file_names[0], 'r') as rf:
+#     lines = rf.readlines()
+#     for l in lines[1:]:
+#         count += 1
+#         wf.write(l)
+# for f in file_names[1:]:
+#     x = "../"+f
+#     with open(x, 'r') as rf:
+#         lines = rf.readlines()
+#         for line in lines[2:]:
+#             count += 1
+#             wf.write(line)
+# print count
+# wf.close()
+
+#print f.readlines()[1].split(',')
+# load the CSV file as a numpy matrix
+# conn = sqlite3.connect(url)
+# #query = '''select count(*) from INFORMATION_SCHEMA.COLUMNS
+# # WHERE table_name = loan'''
+# #query = '''select * from loan where dti_joint is NULL limit 10'''
+#
+# # Creating dataframe from query.Teams = conn.execute(query).fetchall()
+# Teams = conn.execute(query).fetchall()
+# print tabulate(Teams, headers='keys', tablefmt='psql')
